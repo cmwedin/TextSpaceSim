ec8371fbc89483674314b1c55389047f5f37ffc9
cmwedin <cmwedin@gmail.com>
2021-06-09 11:19:45 -0400

    added some basic code for implementing ships

---
+++ Assets/Scripts/ShipComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ShipComponent : ScriptableObject {
+    //variables
+    string componentName;
+    float powerDraw;    
+}
---
+++ Assets/Scripts/ShipComponentSlot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+enum slotSize
+{
+    XS,S,M,L,XL,UM
+}
+
+public class ShipComponentSlot : ScriptableObject
+{
+    slotSize size;
+    float maxPower;
+    public ShipComponent contents;
+}
