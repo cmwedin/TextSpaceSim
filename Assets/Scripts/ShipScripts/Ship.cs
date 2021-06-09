bb4ee20a613a582deef5d8fbf775304a58135a41
cmwedin <cmwedin@gmail.com>
2021-06-09 14:40:11 -0400

    no message

---
+++ Assets/Scripts/ShipScripts/Ship.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : ScriptableObject
+{
+    //variables
+    public float hullWeight;
+    public List<ShipComponentSlot> slots;
+}
