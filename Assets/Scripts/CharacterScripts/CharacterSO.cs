500bfbc7e3fa3362df5b8cfbe14a2264b7c5ff55
cmwedin <cmwedin@gmail.com>
2021-06-23 09:21:49 -0400

    file restrucurting and inital character code

---
+++ Assets/Scripts/CharacterScripts/CharacterSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+struct Atribs
+{
+    public int Strength { get; set; }
+    public int Inteligence { get; set; }
+    public int Reflex { get; set; }
+    public int Vitality { get; set; }
+}
+
+[CreateAssetMenu(fileName = "NewCharacterSO", menuName = "TextSpaceSim/Characters/New Character", order = 0)]
+public class CharacterSO : ScriptableObject 
+{
+    [SerializeField] public bool isPlayer { get; set; }
+}
---
+++ Assets/Scripts/CharacterScripts/Stat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    private int _value;
+    private int _buff;
+    public int Value 
+        { get => _value + _buff; 
+          set => _value = value; }
+}
